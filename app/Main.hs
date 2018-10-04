0db6edc6c2d5a9a20dfcb57170d8387b14cbed77
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2018-10-05 00:02:05 +0300

    Add textbook code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,86 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Lib
+import User
+import Control.Exception
+import Control.Monad (forever)
+import Data.List (intersperse)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.ByteString.UTF8 as UTF8
+
+import Data.Typeable
+import Database.SQLite.Simple hiding (close)
+import qualified Database.SQLite.Simple as SQLite
+import Database.SQLite.Simple.Types
+import Network.Socket hiding (close, recv)
+import qualified Network.Socket as S(close, bind)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Network.Socket.ByteString (recv, sendAll)
+import Text.RawString.QQ
+
+returnUsers :: Connection -> Socket -> IO ()
+returnUsers dbConn soc = do
+  rows <- query_ dbConn allUsers
+  let usernames = map username rows
+      newlineSeparated =
+        T.concat $ intersperse "\n" usernames
+  sendAll soc (encodeUtf8 newlineSeparated)
+
+formatUser :: User -> ByteString
+formatUser (User _ username shell
+            homeDir realName _) = BS.concat
+  ["Login: ", e username, "\t\t\t\t",
+   "Name: ", e realName, "\n",
+   "Directory: ", e homeDir, "\t\t\t",
+   "Shell: ", e shell, "\n"]
+  where e = encodeUtf8
+
+returnUser :: Connection -> Socket -> Text -> IO ()
+returnUser dbConn soc username = do
+  maybeUser <- getUser dbConn (T.strip username)
+  case maybeUser of
+    Nothing -> do
+      let message =
+            "Couldn't find matching user for username: " ++
+              (show username)
+      putStrLn message
+      sendAll soc (UTF8.fromString message)
+      return ()
+    Just user -> sendAll soc (formatUser user)
+
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name -> returnUser dbConn soc (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries dbConn sock = forever $ do
+  (soc, _) <- accept sock
+  putStrLn "Got connection, handling query"
+  handleQuery dbConn soc
+  S.close soc
 
 main :: IO ()
-main = someFunc
+main = withSocketsDo $ do
+  addrinfos <-
+    getAddrInfo
+      (Just (defaultHints {addrFlags = [AI_PASSIVE]}))
+      Nothing (Just "2079")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr)
+          Stream defaultProtocol
+  S.bind sock (addrAddress serveraddr)
+  listen sock 1
+  -- only one connection open at a time
+  conn <- open "finger.db"
+  handleQueries conn sock
+  SQLite.close conn
+  S.close sock
